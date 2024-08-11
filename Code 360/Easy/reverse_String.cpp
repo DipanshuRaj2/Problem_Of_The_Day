7d9b169d70af0578e4f59fa8661b8133bd7c9e03
DipanshuRaj2 <dipanshurajjmt2@gmail.com>
2024-08-11 16:40:35 +0530

    reverse string another approach use recursion

--- Code 360/Easy/reverse_String.cpp
+++ Code 360/Easy/reverse_String.cpp
@@ -1,26 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
+void reverseStr(string& str, int i, int n){
+    if(i >= n)return ;
+    swap(str[i], str[n-1]);
+    reverseStr(str, i+1, n - 1);
+}
 string reverseString(string &str){
+    // first approach
     // reverse(str.begin(), str.end());
     // return str;
 
+    //second approach
     // string temp = "";
     // for(int i = str.size()-1; i >= 0; i--){
     //     temp = temp + str[i];
     // }
     // return temp;
     
-    int i = 0;
-    int j = str.size()-1;
-    while(i <= j){
-        swap(str[i] , str[j]);
-        i++, j--;
-    }
+    // third approach
+    // int i = 0;
+    // int j = str.size()-1;
+    // while(i <= j){
+    //     swap(str[i] , str[j]);
+    //     i++, j--;
+    // }
+    // return str;
+
+    // fourth approach use recursion
+    reverseStr(str, 0, str.size());
     return str;
 }
 int main()
 {
-    string str = "werty";
+    string str = "qwerty";
     cout<<"reverse: "<<reverseString(str);
     return 0;
 }   
