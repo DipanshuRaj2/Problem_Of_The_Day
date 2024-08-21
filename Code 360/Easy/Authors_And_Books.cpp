22869fac592559e1e057f1475fb219fa4db96fc7
DipanshuRaj2 <dipanshurajjmt2@gmail.com>
2024-08-21 22:05:31 +0530

    Authors and books

---
+++ Code 360/Easy/Authors_And_Books.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<string> arrangeAuthors(vector<vector<string>>&s){
+    int n = s.size();
+    vector<string>str;
+    for(int i = 0; i<n; i++){
+        str.push_back(to_string(i+1) + ". "+s[i][0]);
+        for(int j = 1; j<s[i].size(); j++){
+            char letter = 'A' +  (j - 1);
+            string entry = "  " + string(1,letter)+ ". "+ s[i][j];
+            str.push_back(entry);
+        }
+    }
+    cout<<"ans: "<<endl;
+    for(int i = 0; i<str.size(); i++){
+        cout<<str[i]<<" ";
+    }
+    return str;
+}
+int main()
+{
+    vector<vector<string>>s = {{"4","ChetanBhagat", "TwoStates", "Revolution", "HalfGirlfriend", "OneIndianGirl"},{"2", "JKRowling", "HarryPotter", "FantasticBeasts"}};
+    // for(int i = 0; i<s.size(); i++){
+    //     for(int j = 0; j<s[i].size(); j++){
+    //         cout<<s[i][j]<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    vector<string>str = arrangeAuthors(s);
+    // string ch = "A";
+    // string str  = "Abc" + ch;
+    
+}
---
+++ Code 360/Easy/tempCodeRunnerFile.cpp
@@ -0,0 +1,5 @@
+    // for(int i = 0; i<26; i++){
+    //     char ch = 'A' + i;
+    //     string str = string(1, ch);
+    //     cout<<str<<" ";
+    // }
