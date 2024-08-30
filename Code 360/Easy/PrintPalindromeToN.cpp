9e032b11f7be9fbe81d350e94bfae78dd078a551
DipanshuRaj2 <dipanshurajjmt2@gmail.com>
2024-08-30 16:37:10 +0530

    adi

---
+++ Code 360/Easy/PrintPalindromeToN.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h> 
+using namespace std;
+bool check(int n){
+	int ans = n;
+	int res = 0;
+	while(n != 0){
+		int ele = n % 10;
+		res = res * 10 + ele;
+		n /= 10;
+	}
+	if(ans == res) return true;
+	return false;
+
+}
+vector<int> getPalindromes(int n)
+{
+	vector<int>ans;
+	for(int i = 1; i<= n; i++){
+		if(check(i)){
+			ans.push_back(i);
+		}
+	}
+	return ans;
+}
+AIzaSyCOkTMiWjEMHehxvBIKyMRyXDwMnPKYtMY
+int main(){
+    vector<int>ans = getPalindromes(12);
+    for(int i = 0; i<ans.size(); i++){
+        cout<<ans[i] <<" ";
+    }
+    return 0;
+}
