ddaf45a258673631707233beb47a14f1af9cdece
DipanshuRaj2 <dipanshurajjmt2@gmail.com>
2024-09-07 00:21:31 +0530

    Missing NumberIn ap

--- Code 360/Easy/Missing_Number_INAP.cpp
+++ Code 360/Easy/Missing_Number_INAP.cpp
@@ -1,25 +1,40 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int missingNumber(vector<int> &arr, int n) 
+int missingNumber(vector<int> &arr, int n)
 {
-    int n = arr.size();
-   int diff = arr[1] - arr[0];
-   int last = arr[n-1] - arr[n];
-   int cD = 0;
-   if(diff == last){
-    cd = diff;
-   }
-   else{
-    if(arr[2] - arr[1] == diff){
-        cd = last;
-    else if(arr[2] - arr[1] == last){
-        cd =  
+    
+    int Fdiff = arr[1] - arr[0];
+    int last = arr[n - 1] - arr[n-2];
+    int cd = 0;
+    if (Fdiff == last)
+    {
+        cd = Fdiff;
     }
+    else
+    {
+        if (arr[2] - arr[1] == Fdiff)
+        {
+            cd = last;
+        }
+            else if (arr[2] - arr[1] == last)
+            {
+                cd = Fdiff;
+            }
     }
-   }
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i+1] == arr[i]+ cd)
+        {
+            continue;
+        }
+        else
+        {
+            return arr[i] + cd;
+        }
+    }
 }
-int main(){
-    vector<int>arr ={1, 4, 10};
-    cout<<arr
- return 0;
- }
+int main()
+{
+    vector<int> arr = {1, 4, 10};
+    cout<<missingNumber(arr, arr.size());
+}
