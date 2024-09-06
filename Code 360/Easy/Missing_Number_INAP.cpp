19d2491030bb7fcb994fc06c3aef296ccc742e56
DipanshuRaj2 <dipanshurajjmt2@gmail.com>
2024-09-06 23:58:38 +0530

    missing Number in Ap

---
+++ Code 360/Easy/Missing_Number_INAP.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int missingNumber(vector<int> &arr, int n) 
+{
+    int n = arr.size();
+   int diff = arr[1] - arr[0];
+   int last = arr[n-1] - arr[n];
+   int cD = 0;
+   if(diff == last){
+    cd = diff;
+   }
+   else{
+    if(arr[2] - arr[1] == diff){
+        cd = last;
+    else if(arr[2] - arr[1] == last){
+        cd =  
+    }
+    }
+   }
+}
+int main(){
+    vector<int>arr ={1, 4, 10};
+    cout<<arr
+ return 0;
+ }
