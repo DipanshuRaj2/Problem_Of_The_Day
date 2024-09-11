8fef971ca36605694e9209dca11cc2339aafdbe2
DipanshuRaj2 <dipanshurajjmt2@gmail.com>
2024-09-11 13:48:56 +0530

    Finding Binary in Decimal

---
+++ Code 360/Easy/Finding_Binary_in_Decimal.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+long countOfNumbers(long n)
+{
+    long long cnt = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        int ele = i;
+        bool flag = false;
+        while (ele != 0)
+        {
+            int last = ele % 10;
+            if (last == 0 || last == 1)
+            {
+                flag = true;
+            }
+            else{
+                flag = false;
+                break;
+            }
+            ele /= 10;
+        }
+        if (flag == true)
+        {
+            cout << i << "   ";
+            cnt++;
+        }
+    }
+    cout << endl;
+    return cnt;
+}
+int main()
+{
+    cout << countOfNumbers(21);
+    return 0;
+}
