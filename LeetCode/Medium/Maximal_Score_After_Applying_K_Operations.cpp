5822ba5dc7bcf22d349cbc9a78174402dc2fce37
dipkali <dipkali@kali.dipkali>
2024-10-15 02:39:31 +0530

    maximum score after apply k opertions

--- LeetCode/Medium/Maximal_Score_After_Applying_K_Operations.cpp
+++ LeetCode/Medium/Maximal_Score_After_Applying_K_Operations.cpp
@@ -1,9 +1,13 @@
+// leetcode = 2530
 #include<bits/stdc++.h>
 using namespace std;
 class Solution {
 public:
-    int maxiIdx(vector<int>& arr) {
-        int maxidx = -1;
+
+    //brute force 
+    // I am choosing the greatest element in every time and after that I am doing kth opertion
+    int maxiIdx(vector<int>& arr) { //maxIdx
+        int maxidx = -1;  
         int maxi = INT_MIN;
         for (int i = 0; i < arr.size(); i++) {
             if (maxi <= arr[i]) {
@@ -13,12 +17,33 @@ public:
         }
         return maxidx;
     }
-    long long maxKelements(vector<int>& nums, int k) {
+    long long maxKelements(vector<int>& nums, int k) { //doing kth operations
         long long score = 0;
         while (k != 0) {
             int idx = maxiIdx(nums);
             score += nums[idx];
             nums[idx] = ceil(nums[idx] / 3.0);
+            k--;
+        }
+        return score;
+    } 
+    //brute force end;
+
+    //optimal approach
+    //1. i am using heap first of all I am put all element in the heap(priority queue) then all element arrange in the ascending order with the Time complexity of log(n);
+    //2. then I am k the opertions 
+    //pick the top ele. add to score varible
+    //then top ele divide by 3 and push into arr;
+
+    int maxKelements1(vector<int>&arr, int k){
+        priority_queue<int>pq(arr.begin(), arr.end());
+        long score = 0;
+        while(k != 0){
+            int ele = pq.top();
+            pq.pop();
+            score += ele;
+            ele = ceil(ele/3.0);
+            pq.push(ele);
             k--;
         }
         return score;
@@ -28,6 +53,7 @@ int main(){
     Solution* sol = new Solution();
     vector<int>arr = {1,10,3,3,3};
     int k = 3;
-    cout<<sol->maxKelements(arr, k);
+    cout<<sol->maxKelements1(arr, k);
    return 0;
 } 
+
