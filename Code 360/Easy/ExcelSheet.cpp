37d17c6a192d2a0085eb4da567ad0790045eb464
DipanshuRaj2 <dipanshurajjmt2@gmail.com>
2024-10-28 23:24:17 +0530

    such a silly question

--- Code 360/Easy/ExcelSheet.cpp
+++ Code 360/Easy/ExcelSheet.cpp
@@ -2,24 +2,16 @@
 using namespace std;
 long long titleToNumber(string &str)
 {
-    if (str.length() == 1)
-        return str[0] - 'A' + 1;
-    
-    else
-    {
-        long long count = 0;
-        for (int i = 0; i < str.length(); i++)
-        {
-            int len = str[i] - 'A' + 1;
-            int mod = 26 % len;
-            count += len + mod;
-        }
-         return count;
-    }
+  long long result = 0;
+  for(int i = 0; i<str.size(); i++){
+    int value = str[i] - 'A' + 1;
+    result = result * 26  + value;
+  }
+  return result;
 }
 int main()
 {
-    string str = "AZ";
+    string str = "AZ";  
     cout << titleToNumber(str);
     return 0;
 }
