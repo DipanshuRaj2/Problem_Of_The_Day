04d096c320bead47d8e27836eabb5f7d050197a7
DipanshuRaj2 <dipanshurajjmt2@gmail.com>
2024-10-29 16:02:42 +0530

    tower of hanoi

---
+++ Code 360/Easy/Tower_Of_Hanoi.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+void solve(int n, int src, int aux, int des, vector<vector<int>>&ans){
+    if(n == 1){
+        ans.push_back({src, des});
+        return;
+    }
+    solve(n-1, src, des, aux, ans);
+    ans.push_back({src, des});
+    solve(n-1, aux, src, des, ans);
+}
+vector<vector<int>> towerOfHanoi(int n){
+    vector<vector<int>>ans;
+    int src = 1; int aux = 2; int des = 3;
+    solve(n, src, aux, des, ans);
+    return ans;
+}
+int main(){
+    int n = 2;
+    vector<vector<int>>ans = towerOfHanoi(n);
+    for(auto ele : ans){
+        cout<<"{";
+        for(auto i : ele){
+            cout<<i<<",";
+        }
+        cout <<"}";
+    }
+ return 0;
+ }
